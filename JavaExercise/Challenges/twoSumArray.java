bd7dd3b80b2489e3fc0af1cd4b12a8e7af017570
MarcoAurelioJunior <marcoaurelioroquedeoliveirajun@gmail.com>
2024-04-04 20:26:06 -0300

    first commit

---
+++ JavaExercise/Challenges/twoSumArray.java
@@ -0,0 +1,15 @@
+package JavaExercise.Challenges;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class twoSumArray {
+    public static void main(String[] args) {
+        int target = 9;
+        int sum;
+
+        List<Integer> intArray =  new ArrayList<>(Arrays.asList(15, 11, 2, 7));
+
+    }
+}
