9a834f63a52f25e5c8ef87eb57ef61dd930714ee
MarcoAurelioJunior <marcoaurelioroquedeoliveirajun@gmail.com>
2024-04-07 14:33:46 -0300

    twosum

--- JavaExercise/Challenges/twoSumArray.java
+++ JavaExercise/Challenges/twoSumArray.java
@@ -3,13 +3,30 @@ package JavaExercise.Challenges;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 
 public class twoSumArray {
     public static void main(String[] args) {
         int target = 9;
         int sum;
 
-        List<Integer> intArray =  new ArrayList<>(Arrays.asList(15, 11, 2, 7));
+        List<Integer> intArray =  new ArrayList<>(Arrays.asList(2, 11, 15, 7));
+
+        Random random = new Random();
+
+        do {
+            int num1 = intArray.get(random.nextInt(4));
+            int num2 = intArray.get(random.nextInt(4));
+
+            sum = num1 + num2;      
+            
+            int num1Index = intArray.indexOf(num1);
+            int num2Index = intArray.indexOf(num2);
+
+            if (sum == target) {
+                System.out.println("Adding the index " + (num1Index + 1) + " with the index " + (num2Index + 1) + " in this array. We have result: " + sum);
+            }
+        } while (sum != target);
 
     }
 }
